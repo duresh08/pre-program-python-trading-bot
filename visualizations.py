bfc4f8340847e5aeb377ef68b31927adbed0c112
dhruv.suresh <dhruv.suresh@edelweissfin.com>
2025-01-20 15:16:35 +0530

    data merging

--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from pandas.tseries.offsets import DateOffset
 pd.set_option('display.max_columns', None)
 
 path = "sqlite:///C:\\Users\\dhruv.suresh\\Downloads\\data.db\\data.db"
@@ -16,3 +18,15 @@ print("number of unique symbols: {}".format(len(df['symbol'].unique())))
 df.describe()
 
 cols_to_drop = ['id','reported_currency']
+
+df = df.drop(columns = cols_to_drop)
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+df['date'] = pd.to_datetime(df['date'], format = 'mixed')
+df['target_date'] = (df['date'] + DateOffset(months=3))
+
+future_df = df.loc[:, ['date', 'symbol', 'adj_close']]
+future_df = future_df.rename(columns={'adj_close': 'adj_close_3mo',
+                                     'date': 'target_date'})
+
+df = pd.merge(df, future_df, how = 'left', on = ['target_date','symbol'])
+df
